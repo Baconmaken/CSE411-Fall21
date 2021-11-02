d1b289b5ae3481c8db03036f816a8ddc77ff4679
Jooyong Yi <jooyong@unist.ac.kr>
2021-11-02 23:22:44 +0900

    Replaced ANTLR 4.8 generated files with 4.9 files.

--- sjc/src/main/java/sjc/parser/StaticJavaV4Lexer.java
+++ sjc/src/main/java/sjc/parser/StaticJavaV4Lexer.java
@@ -1,4 +1,4 @@
-// Generated from /Users/robby/Repositories/706/f20-706-project/sjc/src/main/java/sjc/parser/StaticJavaV4.g4 by ANTLR 4.8
+// Generated from C:/Users/UNIST_PLaSE/IdeaProjects/compiler-solution/sjc/src/main/java/sjc/parser\StaticJavaV4.g4 by ANTLR 4.9
 package sjc.parser;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
@@ -11,7 +11,7 @@ import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class StaticJavaV4Lexer extends Lexer {
-	static { RuntimeMetaData.checkVersion("4.8", RuntimeMetaData.VERSION); }
+	static { RuntimeMetaData.checkVersion("4.9", RuntimeMetaData.VERSION); }
 
 	protected static final DFA[] _decisionToDFA;
 	protected static final PredictionContextCache _sharedContextCache =
